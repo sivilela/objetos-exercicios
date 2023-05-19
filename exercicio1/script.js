b963df482e4ea2ffdb682b74ccc19207977fd38b
sivilela <sivilela.s@gmail.com>
2023-05-18 22:40:37 -0300

    Exercício de objetos finalizado

---
+++ exercicio1/script.js
@@ -0,0 +1,26 @@
+const sacolao = []
+
+const frutas = {
+fruta1: {  
+  nome: 'Maçã',
+  preco: 5,
+  disponibilidade: true
+},
+
+fruta2: {
+
+  nome: 'Pêra',
+  preco: 9,
+  disponibilidade: false
+},
+
+fruta3: {
+  nome: 'Banana',
+  preco: 7,
+  disponibilidade: true
+}
+}
+sacolao.push(frutas.fruta1)
+sacolao.push(frutas.fruta2)
+sacolao.push(frutas.fruta3)
+console.log(sacolao);
